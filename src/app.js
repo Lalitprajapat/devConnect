a2310e91212319f772c25a1d755fa3c8209aacda
Lalit <lalitprajapat9347@gmail.com>
2025-11-14 04:31:31 +0530

    testing routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.get("/user",(req, res)=>{
+    res.send({firstName:"Akki",lastName:"hnji"})
+});
+
+app.post("/user",(req,res)=>{
+    console.log("data saved");
+    res.send("data saved successfully");
+})
 app.use("/test",(req, res)=>{
     res.send("heloo server");
 });
