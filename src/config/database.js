e2c508d67fe5114e559f30b6976db5ca7ca47a6b
Lalit <lalitprajapat9347@gmail.com>
2025-11-30 17:05:46 +0530

    constants

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-
+const db_URL = require('../utils/constants');
 const connectDB = async () => {
     await mongoose.connect(
-        "mongodb+srv://Tdev_db_user:uQnNmAY8h8PfhgDj@namastedev.qzyhara.mongodb.net/devTinder",
+        db_URL,
     );
 };
 
---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+const db_URL = "mongodb+srv://Tdev_db_user:uQnNmAY8h8PfhgDj@namastedev.qzyhara.mongodb.net/devTinder"
+module.exports = db_URL;
